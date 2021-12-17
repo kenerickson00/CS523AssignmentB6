4f8e4c46a21790d8f0a4f66e5fcb4fb8ac14f9b6
kenerickson00 <ken_erickson@yahoo.com>
2021-12-16 22:35:20 -0500

    get basic enemy movement and agent detection working

--- Platformer/Assets/Scripts/EnemyController.cs
+++ Platformer/Assets/Scripts/EnemyController.cs
@@ -6,8 +6,9 @@ public class EnemyController : MonoBehaviour
 { //basic agent that goes back and forth between two points
     public Transform dest;
     public float waitTime;
-    public bool flip; //go back instead of forward first
+    public float switchTime;
     public float speed;
+    public GameObject agent; //agent model
 
     UnityEngine.AI.NavMeshAgent nm;
     float tolerance = 0.1f;
@@ -17,6 +18,7 @@ public class EnemyController : MonoBehaviour
     private Vector3 destination;
     private bool dir;
     private float curTime;
+    private Vector3 last;
 
     // Start is called before the first frame update
     void Start()
@@ -31,51 +33,136 @@ public class EnemyController : MonoBehaviour
 
         nm.speed = speed;
         nm.SetDestination(destination);
+        last = transform.position;
+        Debug.Log(destination - origin);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (state == 0)
+        if (state == 2)
         {
-            if (dir)
-            {
-                if (Vector3.Distance(transform.position, destination) < tolerance)
+            nm.SetDestination(agent.transform.position);
+            nm.isStopped = false;
+
+            if (Vector3.Distance(last, transform.position) < tolerance)
+            { //check if agent has escaped
+                curTime += Time.deltaTime;
+                if (curTime >= switchTime)
                 {
-                    nm.isStopped = true;
-                    dir = false;
-                    state = 1;
+                    state = 0;
+                    nm.SetDestination(destination);
+                    dir = true;
                     curTime = 0.0f;
-                    //play waiting/turning animation
                 }
+            }
+            last = transform.position;
+        }
+        else
+        {
+            //raycast stuff
+            bool rayhit = false;
+            RaycastHit hit;
+            float raydist = 6.0f; //arbitrary, may need to tweak this value
+            Vector3 middle = transform.position + new Vector3(0, 0.5f, 0);
+            if (Physics.Raycast(middle, transform.TransformDirection(Vector3.forward), out hit, raydist)) 
+            {
+                rayhit = true;
+            }
+            Vector3 or = Quaternion.Euler(0, 45, 0) * transform.TransformDirection(Vector3.forward);
+            Vector3 ol = Quaternion.Euler(0, -22.5f, 0) * transform.TransformDirection(Vector3.forward);
+            Vector3 ir = Quaternion.Euler(0, 22.5f, 0) * transform.TransformDirection(Vector3.forward);
+            Vector3 il = Quaternion.Euler(0, -45, 0) * transform.TransformDirection(Vector3.forward);
+            if (Physics.Raycast(middle, or, out hit, raydist)) 
+            {
+                rayhit = true;
+            }
+            if (Physics.Raycast(middle, ol, out hit, raydist)) 
+            {
+                rayhit = true;
+            }
+            if (Physics.Raycast(middle, ir, out hit, raydist)) 
+            {
+                rayhit = true;
+            }
+            if (Physics.Raycast(middle, il, out hit, raydist)) 
+            {
+                rayhit = true;
+            }
+
+            if (rayhit)
+            {
+                state = 2;
+                curTime = 0.0f;
+                nm.SetDestination(agent.transform.position);
+                nm.isStopped = false;
             }
             else
             {
-                if (Vector3.Distance(transform.position, origin) < tolerance)
+                //other states
+                if (state == 0)
                 {
-                    nm.isStopped = true;
-                    dir = true;
-                    state = 1;
-                    curTime = 0.0f;
-                    //play waiting/turning animation
+                    if (dir)
+                    {
+                        if (Vector3.Distance(transform.position, destination) < tolerance)
+                        {
+                            nm.isStopped = true;
+                            dir = false;
+                            state = 1;
+                            curTime = 0.0f;
+                            //play waiting/turning animation
+                        }
+                    }
+                    else
+                    {
+                        if (Vector3.Distance(transform.position, origin) < tolerance)
+                        {
+                            nm.isStopped = true;
+                            dir = true;
+                            state = 1;
+                            curTime = 0.0f;
+                            //play waiting/turning animation
+                        }
+                    }
+                }
+                else if (state == 1)
+                {
+                    curTime += Time.deltaTime;
+                    if (curTime >= waitTime)
+                    {
+                        curTime = 0.0f;
+                        state = 0;
+                        if (dir)
+                        {
+                            nm.SetDestination(destination);
+                        }
+                        else
+                        {
+                            nm.SetDestination(origin);
+                        }
+                        nm.isStopped = false;
+                    }
                 }
             }
-        } else if(state == 1)
+        }
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if(other.gameObject == agent) //check if agent has gotten too close
         {
-            curTime += Time.deltaTime;
-            if(curTime >= waitTime)
-            {
-                curTime = 0.0f;
-                state = 0;
-                if(dir)
-                {
-                    nm.SetDestination(destination);
-                } else
-                {
-                    nm.SetDestination(origin);
-                }
-                nm.isStopped = false;
-            }
+            state = 2;
+            curTime = 0.0f;
+            nm.SetDestination(agent.transform.position);
+            nm.isStopped = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == agent) //game over, the agent has been caught
+        {
+            Destroy(agent); //should replace this with some death animation or something
         }
     }
 }
