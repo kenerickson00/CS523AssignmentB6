2c3ab02a49beabcc8e9a0ac66d6c6e5de495d693
Kashif <kashifq@scarletmail.rutgers.edu>
2021-12-19 12:28:22 -0500

    Added start screen

---
+++ Platformer/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonHandler : MonoBehaviour
+{
+    public void startGame()
+    {
+        Debug.Log("Start the game");
+        //SceneManager.LoadScene("game");
+    }
+
+    public void startDemo()
+    {
+        SceneManager.LoadScene("testscene");
+    }
+
+    public void endGame()
+    {
+        Application.Quit();
+    }
+}
