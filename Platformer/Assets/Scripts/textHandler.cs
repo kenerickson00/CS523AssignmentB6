665c4dd3ff23e6f8dda798e22adcbab6842b074a
Kashif <kashifq@scarletmail.rutgers.edu>
2021-12-22 11:57:23 -0500

    Start screen updated

---
+++ Platformer/Assets/Scripts/textHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class textHandler : MonoBehaviour
+{
+
+    void OnMouseOver()
+    {
+        GetComponent<Transform>().localScale = new Vector3(1.2f, 1.2f, 1);
+        GetComponent<TextMesh>().color = new Color(1, 0, 0, 1);
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<Transform>().localScale = new Vector3(1, 1, 1);
+        GetComponent<TextMesh>().color = new Color(0, 26f/255f, 169f/255f, 1);
+    }
+
+    void OnMouseDown()
+    {
+        string buttonClicked = GetComponent<TextMesh>().text;
+        if(buttonClicked == "Start Game")
+            SceneManager.LoadScene("Level");
+        else if(buttonClicked == "Demo Area")
+            SceneManager.LoadScene("testscene");
+        else
+        {
+            Debug.Log("Testing");
+            Application.Quit();
+        }
+    }
+}
