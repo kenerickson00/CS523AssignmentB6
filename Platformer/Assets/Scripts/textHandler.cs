09049944e7b0112aebc0e84872f77a52322257d3
Kashif <kashifq@scarletmail.rutgers.edu>
2021-12-23 13:54:25 -0500

    Fixed bug where level 3 button returned to the first level instead of level 3 when hitting try again

--- Platformer/Assets/Scripts/textHandler.cs
+++ Platformer/Assets/Scripts/textHandler.cs
@@ -24,7 +24,7 @@ public class textHandler : MonoBehaviour
         string buttonClicked = GetComponent<TextMesh>().text;
         if(buttonClicked == "Start Game")
         {
-            SceneManager.LoadScene("Level2"); //SceneManager.LoadScene("OtherLevel topdown");
+            SceneManager.LoadScene("OtherLevel topdown");
         }
         else if(buttonClicked == "Demo Area")
             SceneManager.LoadScene("testscene");
